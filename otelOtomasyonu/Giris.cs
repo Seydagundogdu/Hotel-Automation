1ce4672a7af682bed62e7d8c13d43c8b987a9a86
seydagundogdu <seydagundogdu202@gmail.com>
2021-05-18 17:59:03 +0300

    up

--- otelOtomasyonu/Giris.cs
+++ otelOtomasyonu/Giris.cs
@@ -1,16 +1,4 @@
-﻿/****************************************************************************
-** SAKARYA ÜNİVERSİTESİ
-** BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
-** BİLİŞİM SİSTEMLERİ MÜHENDİSLİĞİ BÖLÜMÜ
-** NESNEYE DAYALI PROGRAMLAMA DERSİ
-** 2019-2020 BAHAR DÖNEMİ
-**
-** ÖDEV NUMARASI..........:2 (Proje)
-** ÖĞRENCİ ADI............:Şeyda Gündoğdu
-** ÖĞRENCİ NUMARASI.......:B181200376
-** DERSİN ALINDIĞI GRUP...:A
-****************************************************************************/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
